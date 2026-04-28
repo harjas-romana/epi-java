1a3309c682adfb3a5e568b33164ca97c6a420c3e
Harjas Singh <harjas42@icloud.com>
2026-04-28 11:36:09 +0530

    Problem 10.2

---
+++ BinaryTrees/SymmetricTree.java
@@ -0,0 +1,29 @@
+package BinaryTrees;
+
+import BinaryTrees.BinaryTreeBootCamp.BinaryTreeNode;
+
+public class SymmetricTree {
+    
+    public static boolean isSymmetric(BinaryTreeNode<Integer> root) {
+        
+        if(root == null) {
+            return true;
+        }
+
+        return checkSymmetric(root.left, root.right);
+    }
+
+    public static boolean checkSymmetric(BinaryTreeNode<Integer> left, BinaryTreeNode<Integer> right) {
+
+        if(left == null && right == null) {
+            return true;
+        }
+
+        if(left == null || right == null) {
+            return false;
+        }
+
+        return left.data.equals(right.data) && checkSymmetric(left.right,right.left) && checkSymmetric(left.left, right.right);
+        
+    }
+}
