048fa797d8e9eeb2de02c633e6a0f66cf65388ef
harjasr42 <harjasr42@gmail.com>
2026-05-27 16:02:00 +0530

    feat: abstract Shape interface

---
+++ OOPAndSystemDesign/Shape.java
@@ -0,0 +1,10 @@
+// POLYMORPHISM EXAMPLE - Interface Definition
+// Demonstrates interface-based polymorphism
+
+public interface Shape {
+    double calculateArea();
+
+    double calculatePerimeter();
+
+    void displayInfo();
+}
